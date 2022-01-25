bf7b6d756a9245cd36badf816e6e26acb52f86e0
Jose Castillo <jcastillo@example.com>
2022-01-25 13:38:11 -0500

    add unit test

--- src/test/java/com/example/demo/ProcessOfficeFilesApplicationTests.java
+++ src/test/java/com/example/demo/ProcessOfficeFilesApplicationTests.java
@@ -6,9 +6,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 
 import com.example.demo.controller.ProcessController;
+import com.example.demo.model.FileRequest;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class ProcessOfficeFilesApplicationTests {
@@ -19,6 +21,9 @@ class ProcessOfficeFilesApplicationTests {
 	@Autowired
 	private ProcessController controller;
 
+	@Autowired
+	private TestRestTemplate restTemplate;
+
 	@Test
 	void contextLoads() {
 		assertThat(controller).isNotNull();
@@ -27,7 +32,9 @@ class ProcessOfficeFilesApplicationTests {
 	@Test
 	void controllerTest() throws Exception {
 		String id = "67f4a66f-6d89-41bb-b639-372a078acb43";
-		controller.getImage(id,true);
+		assertThat(this.restTemplate.postForObject("http://localhost:" + port + "/api-files/getFile",
+				FileRequest.builder().idFile(id).build(), String.class))
+						.contains("67f4a66f-6d89-41bb-b639-372a078acb43");
 	}
 
 }
