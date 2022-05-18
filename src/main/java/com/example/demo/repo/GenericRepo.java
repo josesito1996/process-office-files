8f32056f062e5f9dc51e650f6f731e2452a7d66f
josesito1996 <85954217+josesito1996@users.noreply.github.com>
2022-05-17 20:48:46 -0500

    Revert "Feature dev"

--- src/main/java/com/example/demo/repo/GenericRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repo;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.NoRepositoryBean;
-
-@NoRepositoryBean
-public interface GenericRepo<T,ID> extends CrudRepository<T, ID> {
-
-}
